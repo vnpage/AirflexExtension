aa1fc09af18aa4f60ab09453ef1fa26f76fc798a
Tuan Dinh <leonacky@gmail.com>
2024-06-28 14:29:56 +0700

    update

---
+++ AirflexExtension/Classes/AirflexExtension-Swift.h
@@ -0,0 +1,12 @@
+//
+//  AirflexExtension-Swift.h
+//  Pods
+//
+//  Created by Tuan Dinh on 28/6/24.
+//
+
+#ifndef AirflexExtension_Swift_h
+#define AirflexExtension_Swift_h
+
+
+#endif /* AirflexExtension_Swift_h */
